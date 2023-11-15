c5347f9be8afa2b01088aed8dc196d4aec5ca4b5
syaiful-aa <syaiful.salam@agriaku.com>
2023-11-15 22:12:14 +0700

    add compare coverage

---
+++ compare-coverage/index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+
+try {
+  const command = core.getInput('command');
+
+  const action = command.split(' ')[0];
+
+  let options = {
+    'previous_version': '2.18.0',
+    'current_version': '2.19.0',
+    'previous_coverage': '72.87% (7390 of 10142 lines)',
+    'current_coverage': '62.87% (6390 of 10142 lines)',
+    'previous_result': [
+      {
+        'module': 'authentication_module',
+        'result': '90.5% (900 of 1173 lines)'
+      },
+      {
+        'module': 'discovery_module',
+        'result': '86.8% (1063 of 1154 lines)'
+      }
+    ],
+    'current_result': [
+      {
+        'module': 'authentication_module',
+        'result': '94.5% (1109 of 1173 lines)'
+      },
+      {
+        'module': 'discovery_module',
+        'result': '84.8% (1063 of 1254 lines)'
+      },
+      {
+        'module': 'domain_module',
+        'result': '10% (100 of 1000 lines)'
+      }
+    ],
+  };
+  
+  core.setOutput("build_options", JSON.stringify(options));
+} catch (error) {
+  core.setFailed(error.message);
+}
+
