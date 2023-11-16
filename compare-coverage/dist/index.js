e19adbc220281296632b8501c45aa7022176e1ca
syaiful-aa <syaiful.salam@agriaku.com>
2023-11-16 14:38:12 +0700

    up

--- compare-coverage/dist/index.js
+++ compare-coverage/dist/index.js
@@ -26141,56 +26141,11 @@ try {
   console.log(`previousData: ${previousData}`);
   console.log(`currentData: ${currentData}`);
 
-  let previousSummary = {
-    'covered_lines': 7390,
-    'total_lines': 10142,
-    'percentage': 72.87
-  };
+  const previousDataResult = mapInput(previousData)
+  const currentDataResult = mapInput(currentData)
 
-  let currentSummary = {
-    'covered_lines': 6390,
-    'total_lines': 10142,
-    'percentage': 62.87
-  }
-
-  let previousResult = [
-    {
-      'module': 'authentication_module',
-      'covered_lines': 900,
-      'total_lines': 1173,
-      'percentage': 90.5
-    },
-    {
-      'module': 'discovery_module',
-      'covered_lines': 1063,
-      'total_lines': 1154,
-      'percentage': 86.8
-    }
-  ];
-
-  let currentResult = [
-    {
-      'module': 'authentication_module',
-      'covered_lines': 1109,
-      'total_lines': 1173,
-      'percentage': 94.5
-    },
-    {
-      'module': 'discovery_module',
-      'covered_lines': 1063,
-      'total_lines': 1254,
-      'percentage': 84.8
-    },
-    {
-      'module': 'domain_module',
-      'covered_lines': 100,
-      'total_lines': 1000,
-      'percentage': 10
-    }
-  ];
-
-  let comparisonResult = ''
-  let currentAverageCoverage = ''
+  const comparisonResult = compareCoverage(previousDataResult, currentDataResult);
+  const currentAverageCoverage = `average coverage : ${getFormattedAverageCoverage(currentDataResult.summary)}`
 
   core.setOutput('comparison_result', comparisonResult);
   core.setOutput('current_average_coverage', currentAverageCoverage);
@@ -26199,6 +26154,83 @@ try {
 }
 
 
+function compareCoverage(previous, current) {
+  let result = `version: ${previous.version} --> ${current.version}\n`
+
+  const concatModules = Object.keys(previous.coverage).concat(Object.keys(current.coverage));
+  const allModules = concatModules.filter((item, pos) => concatModules.indexOf(item) === pos)
+
+  for (const item of allModules) {
+    let before = 'none';
+    let after = 'none';
+
+    if (item in previous.coverage) {
+      const itemValue = previous.coverage[item];
+      before = `${itemValue.percentage}% (${itemValue.covered_lines} of ${itemValue.total_lines} lines)`;
+    }
+
+    if (item in current.coverage) {
+      const itemValue = current.coverage[item];
+      after = `${itemValue.percentage}% (${itemValue.covered_lines} of ${itemValue.total_lines} lines)`;
+    }
+
+    result += `${item} : ${before} --> ${after}\n`;
+  }
+
+  result += `average coverage : ${getFormattedAverageCoverage(previous.summary)} --> ${getFormattedAverageCoverage(current.summary)}`;
+
+  return result;
+}
+
+function getFormattedAverageCoverage(input) {
+  return `${input.percentage}% (${input.covered_lines} of ${input.total_lines} lines)`;
+}
+
+function mapInput(input) {
+  let inputDataChunks = input.split("-");
+  const version = inputDataChunks[0].split(':')[1].trim();
+  inputDataChunks.shift();
+
+  let result = {
+    'version': version
+  }
+
+  let coverageMap = {};
+  let coveredLines = 0;
+  let totalLines = 0;
+
+  for (const item of inputDataChunks) {
+    const itemChunks = item.split(":").map((x) => x.trim())
+    if (itemChunks.length < 2) {
+      continue;
+    }
+
+    const itemValueChunks = itemChunks[1].split(',').map((x) => x.trim())
+    if (itemValueChunks.length < 3) {
+      continue;
+    }
+
+    coveredLines += parseFloat(itemValueChunks[1])
+    totalLines += parseFloat(itemValueChunks[2])
+
+    coverageMap[itemChunks[0]] = {
+      'percentage': itemValueChunks[0],
+      'covered_lines': itemValueChunks[1],
+      'total_lines': itemValueChunks[2]
+    };
+  }
+
+  const averagePercentage = ((coveredLines / totalLines) * 100).toFixed(2);
+  const summaryMap = {
+    'percentage': averagePercentage,
+    'covered_lines': coveredLines,
+    'total_lines': totalLines
+  };
+
+  result['coverage'] = coverageMap;
+  result['summary'] = summaryMap;
+  return result;
+}
 })();
 
 module.exports = __webpack_exports__;
