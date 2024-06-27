089e68bb047b1a02c537c869944a630882ab39a8
syaiful-aa <syaiful.salam@agriaku.com>
2024-06-27 10:34:49 +0700

    update publish_to

--- cloudsmith-dependencies/dist/index.js
+++ cloudsmith-dependencies/dist/index.js
@@ -29197,69 +29197,75 @@ const yaml = __nccwpck_require__(123);
 
 // Function to convert version from v3 to ">=3.0.0 <4.0.0"
 function convertVersion(version) {
-    const major = parseInt(version.substring(1)); 
-    const nextMajor = major + 1;
-    return `>=${major}.0.0 <${nextMajor}.0.0`;
+	const major = parseInt(version.substring(1));
+	const nextMajor = major + 1;
+	return `>=${major}.0.0 <${nextMajor}.0.0`;
 }
 
 // Function to update dependency section
 function updateDependency(depName, yamlObj, hostedLink) {
-    const depDetails = yamlObj['dependencies'][depName];
-    if (depDetails && depDetails.git) {
-        depDetails.hosted = hostedLink;
-        if (!depDetails.git.ref.startsWith('v')) {
-            depDetails.version = 'any';
-        } else {
-            depDetails.version = convertVersion(depDetails.git.ref);
-        }
-        delete depDetails.git;
-    }
+	const depDetails = yamlObj['dependencies'][depName];
+	if (depDetails && depDetails.git) {
+		depDetails.hosted = hostedLink;
+		if (!depDetails.git.ref.startsWith('v')) {
+			depDetails.version = 'any';
+		} else {
+			depDetails.version = convertVersion(depDetails.git.ref);
+		}
+		delete depDetails.git;
+	}
 }
 
 // Function to find dependencies with git references
 function findDependenciesWithGit(yamlObj) {
-    return Object.keys(yamlObj.dependencies).filter(depName => {
-        return yamlObj['dependencies'][depName] && yamlObj['dependencies'][depName].git;
-    });
+	return Object.keys(yamlObj.dependencies).filter(depName => {
+		return yamlObj['dependencies'][depName] && yamlObj['dependencies'][depName].git;
+	});
 }
 
+// Function to update or add publish_to
+function updatePublishHost(yamlObj, hostedLink) {
+	yamlObj.publish_to = hostedLink;
+}
 
 // Function to process YAML file and update dependencies
 function processYamlFile(hostedLink) {
-    const filename = 'pubspec.yaml';
-    fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading file ${filename}: ${err}`);
-            return;
-        }
+	const filename = 'pubspec.yaml';
+	fs.readFile(filename, 'utf8', (err, data) => {
+		if (err) {
+			console.error(`Error reading file ${filename}: ${err}`);
+			return;
+		}
 
-        try {
-            const yamlObj = yaml.parse(data);
-            const dependencies = findDependenciesWithGit(yamlObj);
+		try {
+			const yamlObj = yaml.parse(data);
+			const dependencies = findDependenciesWithGit(yamlObj);
 
-            dependencies.forEach(depName => {
-                updateDependency(depName, yamlObj, hostedLink);
-            });
+			dependencies.forEach(depName => {
+				updateDependency(depName, yamlObj, hostedLink);
+			});
 
-            // Convert YAML object back to string
-            const updatedYaml = yaml.stringify(yamlObj);
+			updatePublishHost(yamlObj, hostedLink);
 
-            // Write updated YAML back to file
-            fs.writeFile(filename, updatedYaml, 'utf8', (err) => {
-                if (err) {
-                    console.error(`Error writing file ${filename}: ${err}`);
-                    return;
-                }
-                console.log(`Replacement complete. Check the file ${filename}`);
-            });
-        } catch (e) {
-            console.error(`Error parsing YAML in file ${filename}: ${e}`);
-        }
-    });
+			// Convert YAML object back to string
+			const updatedYaml = yaml.stringify(yamlObj);
+
+			// Write updated YAML back to file
+			fs.writeFile(filename, updatedYaml, 'utf8', (err) => {
+				if (err) {
+					console.error(`Error writing file ${filename}: ${err}`);
+					return;
+				}
+				console.log(`Replacement complete. Check the file ${filename}`);
+			});
+		} catch (e) {
+			console.error(`Error parsing YAML in file ${filename}: ${e}`);
+		}
+	});
 }
 
 module.exports = {
-    processYamlFile
+	processYamlFile
 };
 
 
